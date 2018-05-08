7ca2dba855c3b28c3467e9b544a2860280e91b27
Sergey Vinogradov <grapesh@gmail.com>
2018-05-08 18:22:57 -0400

    Update test.bash

--- scripts/test.bash
+++ scripts/test.bash
@@ -11,11 +11,9 @@ export myModules=${platform}"/gpfs/hps3/nos/noscrub/nwprod/csdlpy-1.5.1"
 export pythonCode=${platform}"/gpfs/hps3/nos/noscrub/nwprod/hsofs-preView-1.5.1/preview/pre.py"
 export logFile=${platform}"/gpfs/hps3/nos/noscrub/polar/preview/preview.log"
 
-export aswipPath=${platform}"/gpfs/hps3/nos/noscrub/Yuji.Funakoshi/Dev-hsofs.v2.0.1/exec/hsofs_aswip"
+export trackPath=${platform}"/gpfs/hps3/nos/noscrub/Yuji.Funakoshi/hsofs_preview/"
 export stormID="al802018"
-export stormCycle="2018050712"
-export aDeck=${platform}"/gpfs/gp1/nhc/save/guidance/storm-data/zsurge/al182018/al802018.fst"
-export bDeck=${platform}"/gpfs/gp1/nhc/save/guidance/storm-data/zsurge/bal182018/bal802018.dat"
+export stormCycle="2018050812"
 
 export outputDir=${platform}"/gpfs/hps3/nos/noscrub/polar/preview/"
 export tmpDir=${platform}"/gpfs/hps3/nos/noscrub/tmp/preview/"
@@ -25,4 +23,4 @@ export ftpLogin="svinogradov@emcrzdm"
 export ftpPath="/home/www/polar/estofs/hsofs/preview/"
 
 cd ${tmpDir}
-PYTHONPATH=${myModules} ${pyPath}/python -W ignore ${pythonCode} -w ${aswipPath} -i ${stormID} -c ${stormCycle} -a ${aDeck} -b ${bDeck} -o ${outputDir} -t ${tmpDir} -p ${pltCfgFile} -u ${ftpLogin} -f ${ftpPath} # > ${logFile}
+PYTHONPATH=${myModules} ${pyPath}/python -W ignore ${pythonCode} -w ${trackPath} -i ${stormID} -c ${stormCycle} -o ${outputDir} -t ${tmpDir} -p ${pltCfgFile} -u ${ftpLogin} -f ${ftpPath} # > ${logFile}
